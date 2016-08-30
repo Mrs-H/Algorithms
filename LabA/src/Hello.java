1db3cb66d218dcde0513942e61533a892f3c3d83
Elisa Heinricher <mrs.heinricher@gmail.com>
2016-08-30 14:13:00 -0400

    Lab A - Using Eclipse

---
+++ LabA/src/Hello.java
@@ -0,0 +1,10 @@
+//@author :Elisa Heinricher
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Algorithms");
+	}
+
+}
